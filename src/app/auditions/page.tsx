0ace3baa4bf35bbd97c362a1dafadcf0ee3050b0
alexandros <homocalidus@gmail.com>
2025-02-06 02:23:36 +0100

    setup

---
+++ src/app/auditions/page.tsx
@@ -0,0 +1,5 @@
+export default function Auditions() {
+    return (
+      <>Audicije</>
+    );
+  }
---
+++ src/theme.ts
@@ -0,0 +1,51 @@
+'use client';
+import { createTheme } from '@mui/material/styles';
+
+const defaultTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#3f51b5',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: 'Open Sans',
+    h1: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    h2: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    h3: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    h4: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    h6: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    h5: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    subtitle1: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    subtitle2: {
+      fontFamily: 'Ubuntu Mono',
+    },
+    button: {
+      fontFamily: 'Ubuntu Mono',
+      fontWeight: 900,
+    },
+    overline: {
+      fontFamily: 'Ubuntu Mono',
+    },
+  },
+  cssVariables: true,
+});
+
+export default defaultTheme;
