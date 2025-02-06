254ee604f5d736765b714c5a615eb8082d7797ef
alexandros <homocalidus@gmail.com>
2025-02-06 19:56:40 +0100

    list data - setup

---
+++ src/app/auditions/AuditionsUI.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { AuditionProps } from "@/types/audition";
+import { DataGrid, GridColDef, GridRenderCellParams, GridValueGetter } from "@mui/x-data-grid";
+import { useState } from "react";
+import { format }  from "date-fns";
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { useTheme } from "@mui/material/styles";
+
+
+
+
+export default function AuditionsUI({ error, message, data }: AuditionProps) {
+  const theme = useTheme();
+
+  const columns: GridColDef[] = [
+    {
+      field: "startDate",
+      headerName: "Datum početka",
+      type:'date',
+      valueFormatter: (value)=>{
+        return format(new Date(value), 'dd. M. yyyy.');
+      },
+      flex:1,
+    },
+    {
+      field: "endDate",
+      headerName: "Datum završetka",
+      valueFormatter: (value)=>{
+        console.log(value)
+        if(!value)
+          return ''
+
+        return format(new Date(value), 'dd. M. yyyy.');
+      },
+      flex:1,
+    },
+    {
+      field:"auditionTime",
+      headerName:"Vreme",
+      flex:1,
+      valueFormatter: (value)=>{
+        if(!value)
+          return '';
+        return format(new Date(`1970-01-01T${value}`), 'HH:mm');
+      }
+    },
+    {
+      field: "auditionType",
+      headerName: "Tip audicije",
+      flex:1,
+    },
+    {
+      field: "presentationType",
+      headerName: "Tip prezentacije",
+      flex:1,
+    },
+    {
+      field: "auditionStatus",
+      headerName: "Status",
+      flex:1,
+      renderCell: (params: GridRenderCellParams<any, String>) => {
+        switch (params.value) {
+          case 'in-progress':
+            return(<span style={{color:theme.palette.primary.main}}><b>Aktivna</b></span>)
+            break;
+          case 'draft':
+            return(<span style={{color:theme.palette.secondary.main}}><b>Nacrt</b></span>)
+            break;
+          case 'finished':
+            return(<span style={{color:theme.palette.success.main}}><b>Završena</b></span>)
+            break;
+          default:
+            return(<span style={{color:theme.palette.error.main}}><b>Unknown</b></span>)
+            break;
+        }
+        
+      }
+    },
+  ];
+
+  return (
+    <div style={{ height: 'auto', width: "90%", margin: 'auto'}}>
+      <DataGrid
+        columns={columns}
+        rows={data.rows}
+      />
+    </div>
+  );
+}
--- src/app/auditions/page.tsx
+++ src/app/auditions/page.tsx
@@ -1,5 +1,34 @@
-export default function Auditions() {
+import { AuditionProps } from "@/types/audition";
+import AuditionsUI from "./AuditionsUI";
+
+export default async function Auditions() {
+
+  const urlStr = process.env.API ?? "";
+
+  const auditionProps:AuditionProps = {
+    error:false,
+    message:"",
+    data:{
+      rows:[]
+    }
+  }
+
+  try{
+    const url = new URL(urlStr);
+    url.pathname += 'auditions';
+    const result = await fetch(url);
+    const json = await result.json();
+    auditionProps.error = json.error;
+    auditionProps.message = json.message;
+    auditionProps.data = json.data;
+  } catch(error){
+    auditionProps.error = true;
+    auditionProps.message = "An error has occured while fetching data.";
+    auditionProps.data = {rows:[]};
+  }
+
+  
     return (
-      <>Audicije</>
+      <AuditionsUI {...auditionProps}/>
     );
   }
