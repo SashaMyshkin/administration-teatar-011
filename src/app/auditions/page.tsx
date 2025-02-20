bce2555a7160f243c5fc0517d3964686efbe487a
alexandros <homocalidus@gmail.com>
2025-02-20 18:24:55 +0100

    change started

--- src/app/auditions/page.tsx
+++ src/app/auditions/page.tsx
@@ -1,34 +1,9 @@
 import { AuditionProps } from "@/types/audition";
 import AuditionsUI from "./AuditionsUI";
+import db from "@/db";
+import audition from "@/db/schemas/audition";
 
 export default async function Auditions() {
-
-  const urlStr = process.env.API ?? "";
-
-  const auditionProps:AuditionProps = {
-    error:false,
-    message:"",
-    data:{
-      rows:[]
-    }
-  }
-
-  try{
-    const url = new URL(urlStr);
-    url.pathname += 'auditions';
-    const result = await fetch(url);
-    const json = await result.json();
-    auditionProps.error = json.error;
-    auditionProps.message = json.message;
-    auditionProps.data = json.data;
-  } catch(error){
-    auditionProps.error = true;
-    auditionProps.message = "An error has occured while fetching data.";
-    auditionProps.data = {rows:[]};
-  }
-
-  
-    return (
-      <AuditionsUI {...auditionProps}/>
-    );
-  }
+ // const result = db.select().from(audition);
+  return <></>;
+}
