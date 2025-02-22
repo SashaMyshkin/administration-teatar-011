6c5f47e24d30c0144763ba88339e87f5752d5a17
alexandros <homocalidus@gmail.com>
2025-02-22 03:27:50 +0100

    environments

---
+++ production.bat
@@ -0,0 +1,38 @@
+@echo off
+
+set HOST=192.168.8.152
+set PORT=3030
+
+REM echo Changing directory to D:\Projects\administration-teatar-011
+cd /d "D:\Projects\administration-teatar-011"
+REM echo Current directory: %cd%
+
+REM Install dependencies (if needed)
+REM echo Running npm install...
+call npm install
+REM if errorlevel 1 (
+REM   echo npm install failed!
+REM   pause
+REM   exit /b
+REM )
+
+REM Build the app
+REM echo Running npm run build...
+call npm run build
+REM if errorlevel 1 (
+REM   echo npm run build failed!
+REM   pause
+REM   exit /b
+REM )
+
+REM Start the app
+REM echo Starting the app...
+call npm run start
+REM if errorlevel 1 (
+REM   echo npm run start failed!
+REM   pause
+REM   exit /b
+REM )
+
+REM Pause to keep the window open
+REM pause
